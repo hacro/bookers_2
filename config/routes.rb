9b32c6c1b35d3de987059625ff04b263f99a92e1
hazuki <87082685+hacro@users.noreply.github.com>
2023-10-24 11:12:10 +0900

    削除機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about', as: 'about'
   root to: "homes#top"
 
-  resources :books, only: [:new, :show, :index, :create]
+  resources :books, only: [:new, :show, :index, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
