5628e13cc7ffeb9762e7c9be0597e8e3f7cd5975
hazuki <87082685+hacro@users.noreply.github.com>
2023-10-24 14:04:22 +0900

    User詳細画面の実装、リンクの設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   root to: "homes#top"
 
   resources :books, only: [:new, :show, :index, :create, :destroy]
+  resources :users, only: [:edit, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
