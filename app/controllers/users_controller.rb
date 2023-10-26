0040d729d3057cf078dd1762e07ade6c36f221c8
hazuki <87082685+hacro@users.noreply.github.com>
2023-10-26 15:08:40 +0900

    部分共通テンプレートの実装と

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
@@ -14,10 +15,14 @@ class UsersController < ApplicationController
     redirect_to user_path(current_user.id)
   end
 
+  def index
+    @users = User.all
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about', as: 'about'
   root to: "homes#top"
 
-  resources :books, only: [:new, :show, :index, :create, :destroy]
-  resources :users, only: [:edit, :show, :update]
+  resources :books, only: [:new, :show, :index, :create, :destroy, :edit, :update]
+  resources :users, only: [:edit, :show, :update, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
